4a0ddade8556a20c7185b184968ee48c3789fa09
KumarAbhinav01 <abhinav.bitd@gmail.com>
2023-04-14 23:54:08 +0530

    Responsive Design Added

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,29 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Filter.css';
 
 const Filter = ({ filterItem, uniqueList, setPizzaData, PizzaApi }) => {
+  const [sortBy, setSortBy] = useState(null);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="btn-group">
-                    {uniqueList?.map((data) => {
-                        return (
-                            <button className='btn-group__item'
-                                onClick={() => filterItem(data)}>
-                                {data ? "Veg" : "Non-Veg"}
-                            </button>
-                        )
-                    })}
-                    <button className='btn-group__item'
-                        onClick={() => setPizzaData(PizzaApi)}>
-                        All
-                    </button>
+  const handleSortByPrice = () => {
+    const sortedData = [...PizzaApi].sort((a, b) => a.price - b.price);
+    setPizzaData(sortedData);
+    setSortBy('price');
+  };
 
-                </div>
-            </nav>
-        </>
-    )
-}
+  const handleSortByRating = () => {
+    const sortedData = [...PizzaApi].sort((a, b) => b.rating - a.rating);
+    setPizzaData(sortedData);
+    setSortBy('rating');
+  };
 
-export default Filter
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          {uniqueList?.map((data) => {
+            return (
+              <button
+                key={data}
+                className="btn-group__item"
+                onClick={() => filterItem(data)}
+              >
+                {data ? 'Veg' : 'Non-Veg'}
+              </button>
+            );
+          })}
+          <button
+            className="btn-group__item"
+            onClick={() => {
+              setPizzaData(PizzaApi);
+              setSortBy(null);
+            }}
+          >
+            All
+          </button>
+          <button
+            className={`btn-group__item ${
+              sortBy === 'price' ? 'btn-group__item--active' : ''
+            }`}
+            onClick={handleSortByPrice}
+          >
+            Sort by Price
+          </button>
+          <button
+            className={`btn-group__item ${
+              sortBy === 'rating' ? 'btn-group__item--active' : ''
+            }`}
+            onClick={handleSortByRating}
+          >
+            Sort by Rating
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Filter;
